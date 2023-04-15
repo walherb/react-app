57847c4114da380af1e1b055df1c5dd2c4ac9bf2
Walusimbi Herbert Henry <walherb@gmail.com>
2023-04-15 13:13:08 +0300

    first commit

---
+++ Mosh.jsx
@@ -0,0 +1,20 @@
+// npm create vite@4.1.0
+// react-app { framework: React, variant: TypeScript }
+// cd react-app 
+// npm install  
+// npm run dev 
+
+//! babeljs.io/repl
+
+// npm i bootstrap@5.2.3
+
+// State --> Mutable 
+// Props --> Immutable
+
+// children
+
+// React Developer Tools
+
+// Buttons [ Primary, Secondary, Success, Danger, Warning, Info, Light, Dark, Link ]
+
+

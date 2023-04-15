3f86a16f8d10ecfc50c1b301d470047e6986ee28
Walusimbi Herbert Henry <walherb@gmail.com>
2023-04-15 14:01:16 +0300

    Finished Section 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { useState } from 'react';
 import Alert from './components/Alert';
 import Button from './components/Button';
+// import ListGroup from './components/ListGroup/ListGroup';
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const [alertVisible, setAlertVisibility] = useState(false); // Alert
+
+  // const items = ['New York', 'Los Angeles', 'San Francisco'];
 
   return (
-    <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color='dark' onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
-    </div>
+    <>
+      {/* Alert */}
+      <div>
+        {alertVisible && (
+          <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+        )}
+        <Button color='dark' onClick={() => setAlertVisibility(true)}>
+          My Button
+        </Button>
+      </div>
+
+      <div>
+        {/* <ListGroup heading='Miami' items={items} onSelectItem={()} */}
+      </div>
+    </>
   );
 }
 
